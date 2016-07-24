8d832cbb0c6991cf76e790b5e1190580550aa15f
Sai <sailikesyou@gmail.com>
2016-07-24 08:40:33 -0400

    updated meta.rb

--- cookbooks/tomcatinstall/metadata.rb
+++ cookbooks/tomcatinstall/metadata.rb
@@ -1,6 +1,6 @@
 name             'tomcatinstall'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'None'
+maintainer_email 'mandavasaikrishna91@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures tomcatinstall'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
